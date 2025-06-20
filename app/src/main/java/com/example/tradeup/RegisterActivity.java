1b6b141d62c2d93de190a6764de2591c4a49d176
Leuynuyn <leuyentctv2004@gmail.com>
2025-06-21 00:47:05 +0700

    newlogin

--- app/src/main/java/com/example/tradeup/RegisterActivity.java
+++ app/src/main/java/com/example/tradeup/RegisterActivity.java
@@ -1,14 +1,34 @@
 package com.example.tradeup;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText edtUserName, edtEmail, edtPhoneNumber, edtPassWord;
+    Button btnRegister;
+    ProgressBar progressBarRegister;
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,66 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        addControl();
+        addEvent();
     }
+
+    private void addControl() {
+        mAuth = FirebaseAuth.getInstance();
+        edtUserName = findViewById(R.id.edtUserName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPhoneNumber = findViewById(R.id.edtPhoneNumber);
+        edtPassWord = findViewById(R.id.edtPassWord);
+        btnRegister = findViewById(R.id.btnRegister);
+        progressBarRegister = findViewById(R.id.progressBarRegister);
+    }
+
+    private void addEvent() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBarRegister.setVisibility(View.VISIBLE);
+                String user, pw, phone, email;
+                user = edtUserName.getText().toString();
+                pw = edtPassWord.getText().toString();
+                phone = edtPhoneNumber.getText().toString();
+                email = edtEmail.getText().toString();
+
+                if(TextUtils.isEmpty(user)){
+                    Toast.makeText(RegisterActivity.this, "Enter UserName", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(pw)){
+                    Toast.makeText(RegisterActivity.this, "Enter PassWord", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(phone)){
+                    Toast.makeText(RegisterActivity.this, "Enter PhoneNumber", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(RegisterActivity.this, "Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, pw)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBarRegister.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(RegisterActivity.this, "Register success!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
 }
